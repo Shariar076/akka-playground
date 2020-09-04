f15c7c3b10db6230d8190d99b053305346a4cbdc
Shariar076 <shariar1405076@gmail.com>
2020-09-05 01:30:31 +0600

    fix

--- src/main/python/svd-test.py
+++ src/main/python/svd-test.py
@@ -7,6 +7,5 @@ data = Dataset.load_builtin('ml-100k')
 
 # Use the famous SVD algorithm.
 algo = SVD()
-algo.test()
 # Run 5-fold cross-validation and print results.
 cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
